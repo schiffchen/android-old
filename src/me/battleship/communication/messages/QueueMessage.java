71f1b5c78d980de34b32049c193952d5d2cceef3
Manuel Vögele <mvoegele@arcor.de>
2012-05-25 11:23:50 +0200

    Adding more types of QueueMessages

--- src/me/battleship/communication/messages/QueueMessage.java
+++ src/me/battleship/communication/messages/QueueMessage.java
@@ -18,4 +18,35 @@ public class QueueMessage extends BattleshipMessage
 		root.addSubElement(queueing);
 		addExtension(root);
 	}
+	
+	/**
+	 * Creates a message for pinging the matchmaker
+	 * @param id the queueId
+	 */
+	public QueueMessage(String id)
+	{
+		BattleshipPacketExtension root = new BattleshipPacketExtension(ExtensionElements.BATTLESHIP);
+		BattleshipPacketExtension queueing = new BattleshipPacketExtension(ExtensionElements.QUEUEING);
+		queueing.setAttribute("action", "ping");
+		queueing.setAttribute("id", id);
+		root.addSubElement(queueing);
+		addExtension(root);
+	}
+	
+	/**
+	 * Creates a message for confirming the assingment of an opponent
+	 * 
+	 * @param opponentJID the opponent id
+	 * @param matchId the match id
+	 */
+	public QueueMessage(String opponentJID, String matchId)
+	{
+		BattleshipPacketExtension root = new BattleshipPacketExtension(ExtensionElements.BATTLESHIP);
+		BattleshipPacketExtension queueing = new BattleshipPacketExtension(ExtensionElements.QUEUEING);
+		queueing.setAttribute("action", "assigned");
+		queueing.setAttribute("jid", opponentJID);
+		queueing.setAttribute("mid", matchId);
+		root.addSubElement(queueing);
+		addExtension(root);
+	}
 }
