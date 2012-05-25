83e813db770c1698b37b8bd2647d6f59f458426c
Manuel Vögele <mvoegele@arcor.de>
2012-05-25 17:55:49 +0200

    Show login screen at startup

--- src/me/battleship/Main.java
+++ src/me/battleship/Main.java
@@ -1,20 +1,15 @@
 package me.battleship;
 
-import me.battleship.communication.Connection;
-import me.battleship.communication.JID;
-import me.battleship.communication.MatchmakerConnection;
-import me.battleship.communication.MatchmakerConnection.OpponentAssignedListener;
-
-import org.jivesoftware.smack.XMPPException;
-
+import me.battleship.util.ViewFactory;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
 
 /**
  * The main activity
  * 
- * @author manuel
+ * @author Manuel Vögele
  */
 public class Main extends Activity 
 {
@@ -23,45 +18,20 @@ public class Main extends Activity
 	 */
 	public static final String LOG_TAG = "Main";
 	
-	/**
-	 * The connection
-	 */
-	private Connection connection;
-	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		Log.i(LOG_TAG, "main activity started");
-		setContentView(R.layout.main); 
-		connection = new Connection(new JID(LoginCredentials.USERNAME), 80, LoginCredentials.PASSWORD);
-		try
-		{
-			connection.connect();
-			MatchmakerConnection matchmakerConnection = new MatchmakerConnection(connection);
-			matchmakerConnection.queue(new OpponentAssignedListener()
-			{
-				
-				@Override
-				public void onOpponentAssigned(String jid, String matchId)
-				{
-					System.out.println("Opponent: " + jid);
-					System.out.println("Matchid: " + matchId);
-				}
-			});
-		}
-		catch (XMPPException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		FrameLayout frameLayout = new FrameLayout(this);
+		ViewFactory.createView(R.layout.login, frameLayout, this);
+		ViewManager.initialize(this, frameLayout);
 	}
 
 	@Override
 	protected void onDestroy()
 	{
-		connection.disconnect();
 		super.onDestroy();
 	}
 }
