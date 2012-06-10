357ac59e7a45a53bc46c16d806d22575a5528e0d
Manuel Vögele <mvoegele@arcor.de>
2012-06-10 22:32:10 +0200

    Using RelativeLayout for drawing the game grid

--- src/me/battleship/screen/Game.java
+++ src/me/battleship/screen/Game.java
@@ -5,36 +5,49 @@ import me.battleship.util.ViewFactory;
 import android.app.Activity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class Game implements Screen
 {
-	private static final int SIZE = 10;
-	
+	public static final int SIZE = 10;
 	
 	@Override
-	@SuppressWarnings("null")
 	public View getView(Activity activity)
 	{
 		View root = ViewFactory.createView(R.layout.game, activity);
-		LinearLayout playgroundView = (LinearLayout) root.findViewById(R.id.playgroundGrid);
+		RelativeLayout playgroundView = (RelativeLayout) root.findViewById(R.id.playgroundGrid);
 		for (int y = 0;y < SIZE;y++)
 		{
-			LinearLayout row = new LinearLayout(activity);
-			row.setOrientation(LinearLayout.HORIZONTAL);
-			row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-			playgroundView.addView(row);
 			for (int x = 0;x < SIZE;x++)
 			{
-				GridCellView gridCellView = new GridCellView(activity);
-				gridCellView.setLayoutParams(new android.widget.LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1));
-				gridCellView.setBackgroundResource(R.drawable.border);
-				gridCellView.setOnClickListener(new FieldClickListener(x, y));
-				row.addView(gridCellView);
+				LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+				if (y == 0) {
+					layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+				}
+				else {
+					layoutParams.addRule(RelativeLayout.BELOW, getViewId(x, y - 1));
+				}
+				if (x == 0) {
+					layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+				}
+				else {
+					layoutParams.addRule(RelativeLayout.RIGHT_OF, getViewId(x - 1, y));
+				}
+				View cell = new GridCellView(activity);
+				cell.setLayoutParams(layoutParams);
+				int id = getViewId(x, y);
+				cell.setId(id);
+				cell.setOnClickListener(new FieldClickListener(x, y));
+				cell.setBackgroundResource(R.drawable.border);
+				playgroundView.addView(cell);
 			}
 		}
 		return root;
+	}
+	
+	private int getViewId(int x, int y) {
+		return y * SIZE + x + 1;
 	}
 	
 	private class FieldClickListener implements OnClickListener
--- src/me/battleship/screen/GridCellView.java
+++ src/me/battleship/screen/GridCellView.java
@@ -5,14 +5,18 @@ import android.widget.FrameLayout;
 
 public class GridCellView extends FrameLayout
 {
+	private static int height = 0;
+	
 	public GridCellView(Context context)
 	{
 		super(context);
 	}
-
+	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
 	{
-		super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+		int w = MeasureSpec.getSize(widthMeasureSpec);
+		height = (height < w / Game.SIZE ? w / Game.SIZE : height);
+		super.onMeasure(MeasureSpec.makeMeasureSpec(height, MeasureSpec.getMode(widthMeasureSpec)), MeasureSpec.makeMeasureSpec(height, MeasureSpec.getMode(widthMeasureSpec)));
 	}
 }
