1bd27ccf0b5bcb5a5e3612c2123afa042afba2a6
Manuel Vögele <mvoegele@arcor.de>
2012-06-27 23:11:55 +0200

    Temp class for storing login credentials

---
+++ src/me/battleship/LoginCredentials.java
@@ -0,0 +1,9 @@
+package me.battleship;
+
+import java.util.Random;
+
+public class LoginCredentials
+{
+	public static String USERNAME = "node@domain/resource" + new Random().nextInt();
+	public static String PASSWORD = "password";
+}
